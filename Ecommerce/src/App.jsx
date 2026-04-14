b844befa6f6bae79058aa9ce0419233751d557c7
ABHINAV <abhinvog@gmail.com>
2026-04-14 15:58:47 +0530

    Add product card and updated navbar UI

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import Product_details from './components/product_details'
 
 export default function App() {
   return (
     <div>
+      <Navbar />
+      <Product_details/>
     </div>
   )
 }
