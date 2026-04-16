c0e67079ff235b1a5ab08613f9f3e50226991fa5
Aashna Grover <ashnagrover00@gmail.com>
2026-04-16 09:48:38 +0530

    Reserve page UI done

---
+++ Ecommerce/src/components/Reverse.jsx/LoginModal.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./booking.css";
+import { FaAirbnb } from "react-icons/fa";
+
+const LoginModal = ({ closeModal }) => {
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
+
+  const handleContinue = () => {
+    if (!input) {
+      setError("Please enter email or phone number");
+    } else {
+      alert("Logged in successfully!");
+      closeModal();
+    }
+  };
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal animate">
+
+        {/* CLOSE */}
+        <span className="close-btn" onClick={closeModal}>✕</span>
+
+        {/* LOGO */}
+       <FaAirbnb className="logo-icon" />
+
+
+        <h2 className="title">Log in or sign up</h2>
+
+        {/* INPUT */}
+        <input
+          type="text"
+          placeholder="Phone number or email"
+          className="input"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+            setError("");
+          }}
+        />
+
+        {error && <p className="error">{error}</p>}
+
+        {/* BUTTON */}
+        <button className="continue-btn full" onClick={handleContinue}>
+          Continue
+        </button>
+
+        <div className="divider">
+          <span>or</span>
+        </div>
+
+        {/* SOCIAL LOGIN */}
+        <div className="social-btn">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/281/281764.png"
+            alt="google"
+          />
+          Continue with Google
+        </div>
+
+        <div className="social-btn">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/0/747.png"
+            alt="apple"
+          />
+          Continue with Apple
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
