73d4fb516a90cc45a6b57dec1cdd1cebd22417b8
ABHINAV <abhinvog@gmail.com>
2026-04-20 10:38:08 +0530

    added few changes

--- Ecommerce/src/components/Footer.jsx
+++ Ecommerce/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../style/Footer.css";
+import "../style/footer.css";
 import footerTabs from '../data/footerapi'
 
 const Footer = () => {
